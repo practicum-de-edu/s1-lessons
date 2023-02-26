e09e949c87182f5b6f36d795b881a9511af74a7d
Pavel Kulagov <kulagov@gmail.com>
2023-02-26 23:11:33 +0300

    Add de01040701_create_clients_cluster_metrics_m

--- 4. Подготовка данных витрины/7. Загрузка данных в витрину/Задание 1/submit.py
+++ 4. Подготовка данных витрины/7. Загрузка данных в витрину/Задание 1/submit.py
@@ -4,6 +4,7 @@ import os
 
 from run_checker import submit
 
-DE_S1_TEST = 'de01032008'
 
-submit(os.path.dirname(os.path.abspath(__file__)), DE_S1_TEST)
+submit(
+    os.path.dirname(os.path.abspath(__file__)),
+    'de01040701_create_clients_cluster_metrics_m')
