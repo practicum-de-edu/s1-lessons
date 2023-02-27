fbdcd42ced5338e1f2b18aa8450ed5d957eaafd4
Pavel Kulagov <kulagov@gmail.com>
2023-02-27 14:37:39 +0300

    Add de01040703_add_constraint_fk

--- 4. Подготовка данных витрины/7. Загрузка данных в витрину/Задание 3/submit.py
+++ 4. Подготовка данных витрины/7. Загрузка данных в витрину/Задание 3/submit.py
@@ -4,6 +4,6 @@ import os
 
 from run_checker import submit
 
-DE_S1_TEST = 'de01032203'
-
-submit(os.path.dirname(os.path.abspath(__file__)), DE_S1_TEST)
+submit(
+    os.path.dirname(os.path.abspath(__file__)),
+    'de01040703_add_constraint_fk')
