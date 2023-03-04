ddd05a70a1c61271f847014ff0ca51e20c1498e3
Pavel Kulagov <kulagov@gmail.com>
2023-03-04 11:56:08 +0300

    Add de01070401_load_dm

--- 7. Проектирование и разработка витрины с инкрементальной загрузкой/4. Реализация инкрементальной загрузки в коде/Задание 1/submit.py
+++ 7. Проектирование и разработка витрины с инкрементальной загрузкой/4. Реализация инкрементальной загрузки в коде/Задание 1/submit.py
@@ -4,6 +4,6 @@ import os
 
 from run_checker import submit
 
-DE_S1_TEST = 'de01060401'
-
-submit(os.path.dirname(os.path.abspath(__file__)), DE_S1_TEST)
+submit(
+    os.path.dirname(os.path.abspath(__file__)),
+    'de01070401_load_dm')
